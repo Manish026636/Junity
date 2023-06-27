d60a3f9c858b241c684ab3dc6abe1d6d7dc18957
Manish <my79220@gmail.com>
2023-06-28 00:49:52 +0530

    sidebar updated

---
+++ src/Components/Common/CommitteDetails.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const CommitteDetails = () => {
+  return (
+    <div>
+      <div className='bg-[#FBFBFB] w-auto h-auto pb-3  border-2 border-gray-100 rounded-xl'>
+      <div className='flex justify-center items-center'>
+      <h2 className='text-[#5E5E5E] font-semibold pt-3'>Committee Name</h2>
+      </div>
+      <div className='flex justify-center items-center'>
+      <h2 className='text-[#2194FF] text-xl pt-2 font-bold '>UNHRC</h2>
+      </div>
+      <div className='flex justify-center items-center'>
+      <h2 className='text-[#5E5E5E] font-semibold pt-3'>Session ID</h2>
+      </div>
+      <div className='flex justify-center items-center'>
+      <h2 className='text-[#2194FF] text-xl pt-2 font-bold '># 1</h2>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default CommitteDetails
