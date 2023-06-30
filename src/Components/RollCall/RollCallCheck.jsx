e47dd347635aa345bb0ec1f9d976e7af19353e7c
Manish <my79220@gmail.com>
2023-06-30 21:19:36 +0530

    rollcall updated

--- src/Components/RollCall/RollCallCheck.jsx
+++ src/Components/RollCall/RollCallCheck.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import RollCallResult from './RollCallResult'
 import RollCallSearch from './RollCallSearch'
 import RollCallCountry from './RollCallCountry'
+import RollCallPagination from './RollCallPagination'
 
 const RollCallCheck = () => {
     return (
@@ -11,27 +12,14 @@ const RollCallCheck = () => {
                 <div className='flex justify-center items-center mt-6'>
                     <h1 className='text-3xl font-inter text-[#ADADAD] font-bold'>Roll Call</h1>
                 </div>
-                <div className='flex justify-center mt-2 items-center'>
-                    <div className='lg:mt-4'>
-                        <div class="flex items-center justify-center mx-4">
-                            <div class="w-6 h-6 bg-blue-500 rounded-lg"></div>
-                            <span class="ml-2 text-lg text-[#9F9F9F] font-medium">Present</span>
-                        </div>
-                    </div>
-                    <div className='lg:mt-4'>
-                        <div class="flex items-center justify-center mx-4">
-                            <div class="w-6 h-6 bg-[#00bcd4] rounded-lg"></div>
-                            <span class="ml-2 text-lg text-[#9F9F9F] font-medium">Present & Voting</span>
-                        </div>
-                    </div>
-                </div>
-                <div className='flex justify-center gap-4 mt-4 items-center'>
+                
+                <div className='flex justify-center gap-4 mt-2 items-center'>
                     <div className='lg:mt'>
-                        <Checkbox id="ripple-on" className="border-blue-500" style={{ transform: 'scale(1.3)' }} />
+                        <Checkbox id="ripple-on" className="border-blue-500 bg-[#FFFFFF]" style={{ transform: 'scale(1.3)' }} />
                         <label className='text-lg text-[#9F9F9F] font-semibold'>All Present</label>
                     </div>
                     <div className='lg:mt'>
-                        <Checkbox id="ripple-off" className="border-[#00bcd4]" color="cyan" style={{ transform: 'scale(1.3)' }} />
+                        <Checkbox id="ripple-off" className="border-[#00bcd4] bg-[#FFFFFF]" color="cyan" style={{ transform: 'scale(1.3)' }} />
                         <label className='text-lg text-[#9F9F9F] font-semibold'>All Present & Voting</label>
 
                     </div>
@@ -39,10 +27,16 @@ const RollCallCheck = () => {
                 <div className='mx-4 mt-4'>
                 <RollCallSearch/>
                 </div>
-                <div className='mx-4 mt-4'>
+                <div className='mx-4 mt-2'>
                 <RollCallCountry/>
                 </div>
-               
+                <div className='flex justify-end items-end mx-4 mt-4'>
+                <RollCallPagination/>
+                </div>
+                <div className='text-center mx-4 mt-2'>
+                <button type="button" class="text-white bg-[#2194FF] hover:bg-blue-800 focus:ring-4 focus:ring-blue-300  rounded-lg text-md font-bold px-7 py-2 mr-2 mb-2 ">Submit</button>
+
+                </div>
             </div>
         </div>
     )
--- src/Components/RollCall/RollCallCountry.jsx
+++ src/Components/RollCall/RollCallCountry.jsx
@@ -1,70 +1,77 @@
-import React from 'react'
-import { Checkbox } from '@material-tailwind/react'
+import React from "react";
+import { Checkbox } from "@material-tailwind/react";
 
 const RollCallCountry = () => {
-    return (
-        <div>
-            
-            
-<div class="relative overflow-x-auto">
-<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-            <th scope="col" class="px-6 py-3">
-                Country
-            </th>
-            <th scope="col" class="px-6 py-3">
+  const tableData = [
+    { imageUrl: "india.png", label: "INDIA", id: "ripple-on" },
+    { imageUrl: "usa.png", label: "USA", id: "ripple-on" },
+    { imageUrl: "russia.png", label: "RUSSIA", id: "ripple-on" },
+    { imageUrl: "australlia.png", label: "AUSTRALLIA", id: "ripple-on" },
+    { imageUrl: "france.png", label: "FRANCE", id: "ripple-on" },
+    { imageUrl: "germany.png", label: "GERMANY", id: "ripple-on" },
+    { imageUrl: "russia.png", label: "RUSSIA", id: "ripple-on" },
+    { imageUrl: "australlia.png", label: "AUSTRALLIA", id: "ripple-on" },
+    { imageUrl: "france.png", label: "FRANCE", id: "ripple-on" },
+    { imageUrl: "germany.png", label: "GERMANY", id: "ripple-on" },
+
+
+
+
+  ];
+
+  return (
+    <div style={{ overflowX: "auto", overflowY: "hidden" }}>
+      <div className="relative">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400 rounded-xl">
+          <thead className="text-sm text-gray-700  bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 text-gray-400 py-3">Country</th> {/* Empty th */}
+              <th scope="col" className="px-6 text-gray-400 py-3">
                 Present
-            </th>
-            <th scope="col" class="px-6 py-3">
+              </th>
+              <th scope="col" className="px-6 text-gray-400 py-3">
                 Present & Voting
-            </th>
-           
-        </tr>
-    </thead>
-    <tbody>
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Apple MacBook Pro 17"
-            </th>
-            <td class="px-6 py-4">
-                Silver
-            </td>
-            <td class="px-6 py-4">
-                Laptop
-            </td>
-            
-        </tr>
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Microsoft Surface Pro
-            </th>
-            <td class="px-6 py-4">
-                White
-            </td>
-            <td class="px-6 py-4">
-                Laptop PC
-            </td>
-           
-        </tr>
-        <tr class="bg-white dark:bg-gray-800">
-            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                Magic Mouse 2
-            </th>
-            <td class="px-6 py-4">
-                Black
-            </td>
-            <td class="px-6 py-4">
-                Accessories
-            </td>
-           
-        </tr>
-    </tbody>
-</table>
-</div>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableData.map((item, index) => (
+              <tr
+                key={index}
+                className=" border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <td className="px-6 py-1">
+                  <div className="flex items-center">
+                    <img
+                      src={item.imageUrl}
+                      alt={item.label}
+                      className="w-8 h-8 border border-gray-300 rounded-full"
+                    />
+                    <span className="ml-3 text-[#6F6F6F] font-bold text-lg">{item.label}</span>
+                  </div>
+                </td>
+                <td className="px-6 py-1">
+                  <Checkbox
+                    id={item.id}
+                    className="border-[#ECECEC] bg-[#FFFFFF]"
+                    style={{ transform: "scale(1.3)" }}
+                  />
+                </td>
+                <td className="px-6 py-1">
+                  <Checkbox
+                    id={item.id}
+                    className="border-[#ECECEC] bg-[#FFFFFF]"
+                    color="cyan"
+                    style={{ transform: "scale(1.3)" }}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default RollCallCountry
+export default RollCallCountry;
