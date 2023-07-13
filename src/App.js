766b5276cd1f720b7ba3952f6246679fd914d0ad
Manish <my79220@gmail.com>
2023-07-13 14:44:56 +0530

    updation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Dashboard from './Components/ChairDashboard/Dashboard';
+import CommitteDetails from './Components/Common/CommitteDetails';
 import Sidebar from './Components/Common/Sidebar';
 import GSLDashboard from './Components/GSL/GSLDashboard';
 import RollCallDashboard from './Components/RollCall/RollCallDashboard';
@@ -11,7 +12,7 @@ function App() {
     <div className="flex">
     <Sidebar />
     <div className="flex-grow">
-      <GSLDashboard />
+      <Dashboard />
     </div>
   </div>
     
--- src/Components/ChairDashboard/Dashboard.jsx
+++ src/Components/ChairDashboard/Dashboard.jsx
@@ -7,11 +7,11 @@ const Dashboard = () => {
                 <h1 className="text-4xl mt-36  font-bold text-gray-400">Greetings, Chairperson</h1>
             </div>
             <div className="flex gap-8 justify-center items-center">
-                <div className='bg-gradient-to-r  text-center from-gray-700 via-gray-700 to-gray-600 mt-12 w-96 h-80 rounded-2xl '>
+                <div className=' bg-gray-800 text-center mt-12 w-96 h-80 rounded-2xl '>
                 <h1 className='text-xl text-white font-semibold pt-24'>Committee Name</h1>
                 <h1 className='text-4xl text-[#6FF6FF]  font-bold pt-10'>UNHRC</h1>
                 </div>
-                <div className='bg-gradient-to-r text-center from-gray-700 via-gray-700 to-gray-600 mt-12 w-96 h-80 rounded-2xl '>
+                <div className='text-center bg-gray-800 mt-12 w-96 h-80 rounded-2xl '>
                 <h1 className='text-xl text-white font-semibold pt-24'>Total Members</h1>
                 <h1 className='text-4xl text-[#6FF6FF] font-bold pt-10'>50</h1>
 
